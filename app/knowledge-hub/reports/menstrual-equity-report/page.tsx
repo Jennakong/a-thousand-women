88df9064707da2bb34db2b64d012e4217e8ddfca
Jennakong <akongjennifer@gmail.com>
2026-07-31 01:17:53 +0100

    Remove unfinished story page

---
+++ app/knowledge-hub/reports/menstrual-equity-report/page.tsx
@@ -0,0 +1,25 @@
+import Container from "@/components/layout/Container";
+
+export default function AnnualImpactReportPage() {
+
+  return (
+
+    <main>
+
+      <Container>
+
+        <h1>
+          Annual Impact Report
+        </h1>
+
+        <p>
+          Our annual impact report will be available soon.
+        </p>
+
+      </Container>
+
+    </main>
+
+  );
+
+}
